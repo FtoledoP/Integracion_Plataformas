93275bfe607d83b07804119fec4f1c965eb03a88
FrnacoToledo <ftoledo@maimag.cl>
2024-06-07 00:49:14 -0400

    first commiteoooo

---
+++ API-Csharp/InversionesLtda.API/Models/Producto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InversionesLtda.API.Models
+{
+    public class Producto
+    {
+        public int sku { get; set; }
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+        public int stock { get; set; }
+        public int precio { get; set; }
+
+    }
+}
