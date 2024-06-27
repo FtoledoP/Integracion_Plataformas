c059cd014d170aa96c1cf1e7339374a038cabfa6
FrnacoToledo <fr.toledop@duocuc.cl>
2024-06-27 01:14:21 -0400

    update1

--- Integracion/src/app/components/comprar/comprar.component.ts
+++ Integracion/src/app/components/comprar/comprar.component.ts
@@ -1,94 +1,97 @@
-import { Component, ElementRef, TemplateRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, TemplateRef, ViewChild, OnInit } from '@angular/core';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ApiService } from 'src/app/services/api.service';
+import { ToastrService } from 'ngx-toastr';
+import { Flowbite } from '../decorador';
 
 @Component({
   selector: 'app-comprar',
   templateUrl: './comprar.component.html',
   styleUrls: ['./comprar.component.scss']
 })
-export class ComprarComponent {
+@Flowbite()
+export class ComprarComponent implements OnInit {
   @ViewChild('boletaCliente') boletaCliente: TemplateRef<any> | undefined;
   public ventaForm: FormGroup;
-  selectedCliente:any;
-  clientes:any
-  selectedProducts:any[] = [];
-  selectedMethod:any;
-  selectedPayment:any;
+  selectedCliente: any;
+  clientes: any;
+  selectedProducts: any[] = [];
+  selectedMethod: any;
+  selectedPayment: any;
   public modalRef: BsModalRef | undefined;
-  dataSale:any
-  loading:boolean = false;
+  dataSale: any;
+  loading: boolean = false;
+  totalPrice: number = 0; // Precio total de todos los productos en el carrito
 
   constructor(
     private fb: FormBuilder,
     public apiService: ApiService,
-    public bsModalRef: BsModalRef,
     private modalService: BsModalService,
-  ){
+    private toastr: ToastrService
+  ) {
     this.ventaForm = this.fb.group({
-      cliente: ['', Validators.required],
+      customer: ['', Validators.required],
+      shipment: ['', Validators.required],
+      payment: ['', Validators.required]
     });
   }
 
-  ngOnInit(){
+  ngOnInit() {
     this.apiService.getClientes();
     this.apiService.getProductos();
+    this.loadCart();
   }
 
-  async guardarVenta(){
+  loadCart() {
+    const storedProducts = localStorage.getItem('selectedProducts');
+    if (storedProducts) {
+      this.selectedProducts = JSON.parse(storedProducts);
+      this.calculateTotals(); // Calcular los totales iniciales
+    }
+  }
+
+  removeFromCart(product: any) {
+    this.selectedProducts = this.selectedProducts.filter(p => p.sku !== product.sku);
+    localStorage.setItem('selectedProducts', JSON.stringify(this.selectedProducts));
+    this.calculateTotals();
+  }
+
+  addToCart(product: any) {
+    const productExists = this.selectedProducts.some(p => p.sku === product.sku);
+    if (productExists) {
+      this.toastr.error('Este producto ya está en el carrito');
+    } else {
+      product.cantidad = 1;
+      product.precioTotal = product.precio;
+      this.selectedProducts.push(product);
+      localStorage.setItem('selectedProducts', JSON.stringify(this.selectedProducts));
+      this.toastr.success('Se ha agregado ' + product.nombre + ' al carrito', 'Exito');
+      this.calculateTotals();
+    }
+  }
+
+  guardarVenta() {
     this.modalRef?.hide();
     const objEnviar = {
       id_cliente: this.ventaForm.value.cliente,
       productos: this.selectedProducts,
       metodo_envio: this.selectedMethod,
       metodo_pago: this.selectedPayment
-    }
+    };
     console.log('OBJ ENVIAR ---->', objEnviar);
-    this.dataSale = await this.apiService.generarBoleta(objEnviar)
-    console.log('DATA SALE ---->', this.dataSale);
-    this.openBoletaModal();
+    this.apiService.generarBoleta(objEnviar).then(data => {
+      this.dataSale = data;
+      console.log('DATA SALE ---->', this.dataSale);
+      this.openBoletaModal();
+    });
   }
 
-  toggleProductSelection(id: any, event: any) {
-    if (event.target.checked) {
-      this.selectedProducts.push({ id: id, cantidad: 0 });
-    } else {
-      this.removeProduct(id);
-      this.resetQuantityInput(id);
-    }
-  }
-
-  updateQuantity(id: any, event: any) {
-    const index = this.selectedProducts.findIndex(item => item.id === id);
-    if (index !== -1) {
-      this.selectedProducts[index].cantidad = parseInt(event.target.value);
-    }
-  }
-
-  removeProduct(id: number) {
-    const index = this.selectedProducts.findIndex(item => item.id === id);
-    if (index !== -1) {
-      this.selectedProducts.splice(index, 1);
-    }
-  }
-
-  resetQuantityInput(id: number) {
-    const inputElement = document.getElementById('cantidad_' + id) as HTMLInputElement;
-    if (inputElement) {
-      inputElement.value = '';
-    }
-  }
-
-  isProductSelected(id: any): boolean {
-    return this.selectedProducts.some(item => item.id === id);
-  }
-
-  getSelectedMethod(event:any){
+  getSelectedMethod(event: any) {
     this.selectedMethod = event.target.value;
   }
 
-  getSelectedPayment(event:any){
+  getSelectedPayment(event: any) {
     this.selectedPayment = event.target.value;
   }
 
@@ -102,16 +105,48 @@ export class ComprarComponent {
     setTimeout(() => {
       this.loading = false;
       setTimeout(() => {
-        this.guardarVenta()
+        this.guardarVenta();
       }, 2000);
     }, 3500);
     this.modalRef = this.modalService.show(template, { class: 'modal-lg' });
   }
-  
+
   openBoletaModal() {
     if (this.boletaCliente) {
       this.modalRef = this.modalService.show(this.boletaCliente, { class: 'modal-lg' });
     }
+  }
+
+  // Aumentar la cantidad del producto en el carrito
+  increaseQuantity(item: any) {
+    if(item.cantidad >= item.stock) return;
+    item.cantidad++;
+    item.precioTotal = item.precio * item.cantidad; // Actualizar el precio total
+    this.calculateTotals();
+  }
+
+// Disminuir la cantidad del producto en el carrito
+  decreaseQuantity(item: any) {
+    if (item.cantidad > 1) {
+        item.cantidad--;
+        item.precioTotal = item.precio * item.cantidad; // Actualizar el precio total
+        this.calculateTotals();
+    }
+  }
+
+// Actualizar el almacenamiento local con los productos modificados
+  updateLocalStorage() {
+    localStorage.setItem('selectedProducts', JSON.stringify(this.selectedProducts));
+    localStorage.setItem('totalPrice', this.totalPrice.toString())
+  }
+
+  calculateTotals() {
+    this.totalPrice = this.selectedProducts.reduce((total, item) => total + (item.precioTotal), 0);
+    this.updateLocalStorage(); // Actualizar el almacenamiento local después de calcular
+  }
+
+  comprar(){
+    console.log(this.ventaForm.value)
   }
 
 }
